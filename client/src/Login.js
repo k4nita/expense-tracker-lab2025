84f4b8fe6fd7992cefe10f34031a0b117210a93a
erinnasalihu <erinasalihu@hotmail.com>
2025-05-11 02:05:48 +0200

    Small Updates

---
+++ client/src/Login.js
@@ -0,0 +1,10 @@
+import React from 'react';
+
+function Login() {
+    return (
+        <div className="min-h-screen flex items-center justify-center bg-gray-100">
+            <h1 className="text-2xl font-bold">Login Page</h1>
+        </div>
+    );
+}
+export default Login;
