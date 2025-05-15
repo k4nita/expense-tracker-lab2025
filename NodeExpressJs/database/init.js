8b382c240a7eb2cf03c078374cc8c60ab0bd79e8
leutrim307 <trimitrimi299@gmail.com>
2025-05-15 16:02:09 +0200

    Update database credentials in .env file and remove database name from initialization logic

--- NodeExpressJs/database/init.js
+++ NodeExpressJs/database/init.js
@@ -7,7 +7,6 @@ const initializeDatabase = () => {
     host: process.env.DB_HOST,
     user: process.env.DB_USER,
     password: process.env.DB_PASSWORD,
-    database: process.env.DB_NAME,
     port: process.env.DB_PORT
   });
 
